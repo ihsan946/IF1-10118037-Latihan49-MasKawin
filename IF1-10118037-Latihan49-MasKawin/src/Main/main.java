23eb1a3ba668115f4186bd7d060a72b527c324b1
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-10-20 10:42:22 +0700

    Add files via upload

---
+++ IF1-10118037-Latihan49-MasKawin/src/Main/main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author Muhammad Ihsan
+ * NAMA       : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM          : 10118037
+ * Deskripsi Program   : Program ini berisi program biaya emas kawin
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EmasKawin mkawin = new EmasKawin();
+        mkawin.tampilMahar();
+        
+    }
+    
+}
